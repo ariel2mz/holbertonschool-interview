9c11e792f754adb259868fa233d0ca0a96c84bfe
ariel <ariel@gmail.com>
2025-04-08 13:43:36 -0300

    sadsa

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -18,8 +18,8 @@ listint_t *insert_node(listint_t **head, int number)
     while (current != NULL) {
         i++;
         if (current->next == NULL) {
-            current->next == new;
-            new->next == NULL;
+            current->next = new;
+            new->next = NULL;
         }
         if (current->next->n > number) {
             new->next = current->next;
