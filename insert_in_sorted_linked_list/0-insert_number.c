0fe352b1105996745ef1215febf8064a814069a5
ariel <ariel@gmail.com>
2025-04-08 13:42:48 -0300

    sadas

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -7,8 +7,8 @@ listint_t *insert_node(listint_t **head, int number)
 {
     listint_t *current;
     listint_t *new;
-    int i
-    
+    int i;
+
     current = *head;
     new = malloc(sizeof(listint_t));
     if (new == NULL)
