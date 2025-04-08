d53ed6b3fafa94c9737a7a911f9a1f45e57493dd
ariel <ariel@gmail.com>
2025-04-08 13:50:14 -0300

    adsa

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -2,30 +2,30 @@
 #include <stdlib.h>
 #include "lists.h"
 
-
 listint_t *insert_node(listint_t **head, int number)
 {
+    listint_t *new_node;
     listint_t *current;
-    listint_t *new;
-    int i;
+
+    new_node = malloc(sizeof(listint_t));
+    if (new_node == NULL)
+        return (NULL);
+
+    new_node->n = number;
+    new_node->next = NULL;
+
+    if (*head == NULL || number < (*head)->n) {
+        new_node->next = *head;
+        *head = new_node;
+        return new_node;
+    }
 
     current = *head;
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
-        return (NULL);
-    new->n = number;
-    i = 0;
-    while (current != NULL) {
-        i++;
-        if (current->next == NULL) {
-            current->next = new;
-            new->next = NULL;
-        }
-        if (current->next->n > number) {
-            new->next = current->next;
-            current->next = new;
-        }
-    }
-    return new;
+    while (current->next != NULL && current->next->n < number)
+        current = current->next;
 
+    new_node->next = current->next;
+    current->next = new_node;
+
+    return new_node;
 }
