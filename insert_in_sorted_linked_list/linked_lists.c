f39261fd392682f88675c0e341df434432c329b2
ariel <ariel@gmail.com>
2025-04-08 13:40:19 -0300

    sadsa

--- insert_in_sorted_linked_list/linked_lists.c
+++ insert_in_sorted_linked_list/linked_lists.c
@@ -72,24 +72,3 @@ void free_listint(listint_t *head)
         free(current);
     }
 }
-listint_t *insert_node(listint_t **head, int number)
-{
-    current = *head;
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
-        return (NULL);
-    new->n = number;
-    i = 0;
-    while (current != NULL) {
-        i++;
-        if (current->next == NULL) {
-            current->next == new;
-            new->next == NULL;
-        }
-        if (current->next->n > number) {
-            new->next = current->next;
-            current->next = new;
-        }
-    } 
-
-}
