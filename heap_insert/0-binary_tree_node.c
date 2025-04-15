450818aab81b12ddd1dffc0934c209f929b17346
ariel <ariel@gmail.com>
2025-04-15 13:07:47 -0300

    sadsa

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -1,5 +1,11 @@
 #include <stdlib.h>
 #include "binary_trees.h"
+/**
+ * binary_tree_node - sadsadsadsa
+ * @parent: sadsadsa
+ * @value: asdsadsadsa
+ * Return: node
+ */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
