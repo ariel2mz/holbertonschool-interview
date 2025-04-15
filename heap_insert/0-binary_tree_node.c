941b511022cefa77c675465affa4213bf88d1a9f
ariel <ariel@gmail.com>
2025-04-15 12:29:55 -0300

    kjjk

---
+++ heap_insert/0-binary_tree_node.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    binary_tree_t *new_node;
+    new_node->parent = parent
+    new_node->value = value
+}
