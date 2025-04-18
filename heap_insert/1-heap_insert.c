9224efea73e121f9d5731152f95eb58f01714d74
ariel <ariel@gmail.com>
2025-04-18 17:44:59 -0300

    asdadsa

--- heap_insert/1-heap_insert.c
+++ heap_insert/1-heap_insert.c
@@ -10,9 +10,9 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    if (!tree)
-        return (0);
-    return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+if (!tree)
+return (0);
+return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
 
 /**
@@ -23,29 +23,29 @@ size_t binary_tree_size(const binary_tree_t *tree)
  */
 binary_tree_t *get_parent_by_index(binary_tree_t *root, size_t index)
 {
-    if (index == 1)
-        return (NULL);
+if (index == 1)
+return (NULL);
 
-    binary_tree_t *parent = root;
-    size_t path[64];
-    int i = 0;
+binary_tree_t *parent = root;
+size_t path[64];
+int i = 0;
 
-    index = index >> 1;
-    while (index > 1)
-    {
-        path[i++] = index % 2;
-        index >>= 1;
-    }
+index = index >> 1;
+while (index > 1)
+{
+path[i++] = index % 2;
+index >>= 1;
+}
 
-    while (i--)
-    {
-        if (path[i] == 0)
-            parent = parent->left;
-        else
-            parent = parent->right;
-    }
+while (i--)
+{
+if (path[i] == 0)
+parent = parent->left;
+else
+parent = parent->right;
+}
 
-    return (parent);
+return (parent);
 }
 
 /**
@@ -54,18 +54,18 @@ binary_tree_t *get_parent_by_index(binary_tree_t *root, size_t index)
  */
 void swap_up(heap_t **node)
 {
-    heap_t *current = *node;
-    int temp;
+heap_t *current = *node;
+int temp;
 
-    while (current->parent && current->n > current->parent->n)
-    {
-        temp = current->n;
-        current->n = current->parent->n;
-        current->parent->n = temp;
+while (current->parent && current->n > current->parent->n)
+{
+temp = current->n;
+current->n = current->parent->n;
+current->parent->n = temp;
 
-        current = current->parent;
-    }
-    *node = current;
+current = current->parent;
+}
+*node = current;
 }
 
 /**
@@ -76,31 +76,31 @@ void swap_up(heap_t **node)
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
-    if (!root)
-        return (NULL);
+if (!root)
+return (NULL);
 
-    if (*root == NULL)
-    {
-        *root = binary_tree_node(NULL, value);
-        return (*root);
-    }
+if (*root == NULL)
+{
+*root = binary_tree_node(NULL, value);
+return (*root);
+}
 
-    size_t size = binary_tree_size(*root) + 1;
-    binary_tree_t *parent = get_parent_by_index(*root, size);
-    heap_t *new_node = NULL;
+size_t size = binary_tree_size(*root) + 1;
+binary_tree_t *parent = get_parent_by_index(*root, size);
+heap_t *new_node = NULL;
 
-    if (!parent)
-        return (NULL);
+if (!parent)
+return (NULL);
 
-    new_node = binary_tree_node(parent, value);
-    if (!new_node)
-        return (NULL);
+new_node = binary_tree_node(parent, value);
+if (!new_node)
+return (NULL);
 
-    if (!parent->left)
-        parent->left = new_node;
-    else
-        parent->right = new_node;
+if (!parent->left)
+parent->left = new_node;
+else
+parent->right = new_node;
 
-    swap_up(&new_node);
-    return (new_node);
+swap_up(&new_node);
+return (new_node);
 }
