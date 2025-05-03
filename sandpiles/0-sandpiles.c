228b37f21c99f81da8706eb83c002e206f827abd
ariel <ariel@gmail.com>
2025-05-03 01:46:03 -0300

    dsfs

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -9,20 +9,20 @@
  *
  */
 void print_grido(int grid[3][3])
- {
-     int i, j;
-    printf("=\n");
-     for (i = 0; i < 3; i++)
-     {
-         for (j = 0; j < 3; j++)
-         {
-             if (j)
-                 printf(" ");
-             printf("%d", grid[i][j]);
-         }
-         printf("\n");
-     }
- }
+{
+int i, j;
+printf("=\n");
+for (i = 0; i < 3; i++)
+{
+for (j = 0; j < 3; j++)
+{
+if (j)
+printf(" ");
+printf("%d", grid[i][j]);
+}
+printf("\n");
+}
+}
 
 /**
  * is_unstable - Checks if a sandpile is unstable
@@ -32,17 +32,17 @@ void print_grido(int grid[3][3])
  */
 int is_unstable(int grid[3][3])
 {
-    int i, j;
+int i, j;
 
-    for (i = 0; i < SIZE; i++)
-    {
-        for (j = 0; j < SIZE; j++)
-        {
-            if (grid[i][j] > 3)
-                return (1);
-        }
-    }
-    return (0);
+for (i = 0; i < SIZE; i++)
+{
+for (j = 0; j < SIZE; j++)
+{
+if (grid[i][j] > 3)
+return (1);
+}
+}
+return (0);
 }
 
 /**
@@ -51,35 +51,35 @@ int is_unstable(int grid[3][3])
  */
 void topple(int grid[3][3])
 {
-    int temp[3][3] = {{0}};
-    int i, j;
+int temp[3][3] = {{0}};
+int i, j;
 
-    for (i = 0; i < SIZE; i++)
-    {
-        for (j = 0; j < SIZE; j++)
-        {
-            if (grid[i][j] > 3)
-            {
-                temp[i][j] -= 4;
-                if (i > 0)
-                    temp[i - 1][j] += 1;
-                if (i < SIZE - 1)
-                    temp[i + 1][j] += 1;
-                if (j > 0)
-                    temp[i][j - 1] += 1;
-                if (j < SIZE - 1)
-                    temp[i][j + 1] += 1;
-            }
-        }
-    }
+for (i = 0; i < SIZE; i++)
+{
+for (j = 0; j < SIZE; j++)
+{
+if (grid[i][j] > 3)
+{
+temp[i][j] -= 4;
+if (i > 0)
+temp[i - 1][j] += 1;
+if (i < SIZE - 1)
+temp[i + 1][j] += 1;
+if (j > 0)
+temp[i][j - 1] += 1;
+if (j < SIZE - 1)
+temp[i][j + 1] += 1;
+}
+}
+}
 
-    for (i = 0; i < SIZE; i++)
-    {
-        for (j = 0; j < SIZE; j++)
-        {
-            grid[i][j] += temp[i][j];
-        }
-    }
+for (i = 0; i < SIZE; i++)
+{
+for (j = 0; j < SIZE; j++)
+{
+grid[i][j] += temp[i][j];
+}
+}
 }
 
 /**
@@ -89,19 +89,19 @@ void topple(int grid[3][3])
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-    int i, j;
+int i, j;
 
-    for (i = 0; i < SIZE; i++)
-    {
-        for (j = 0; j < SIZE; j++)
-        {
-            grid1[i][j] += grid2[i][j];
-        }
-    }
+for (i = 0; i < SIZE; i++)
+{
+for (j = 0; j < SIZE; j++)
+{
+grid1[i][j] += grid2[i][j];
+}
+}
 
-    while (is_unstable(grid1))
-    {
-        print_grido(grid1);
-        topple(grid1);
-    }
+while (is_unstable(grid1))
+{
+print_grido(grid1);
+topple(grid1);
+}
 }
