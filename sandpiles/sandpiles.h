e3bbf6530ca0c6498dbf0ddf63b7beb4628948c2
ariel <ariel@gmail.com>
2025-05-03 01:30:39 -0300

    klk

--- sandpiles/sandpiles.h
+++ sandpiles/sandpiles.h
@@ -2,7 +2,7 @@
 #define SANDPILES_H
 
 /* Function prototype */
-void print_grid(int grid[3][3]);
+static void print_grid(int grid[3][3]);
 int is_unstable(int grid[3][3]);
 void topple(int grid[3][3]);
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
