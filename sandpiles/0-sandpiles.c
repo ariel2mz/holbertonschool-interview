74ac53209b3b97dd8d11b6f2f7bfb94eeb88bb65
ariel <ariel@gmail.com>
2025-05-03 01:17:28 -0300

    sadas

---
+++ sandpiles/0-sandpiles.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+
+#define SIZE 3
+
+/**
+ * print_grid - Prints a 3x3 grid
+ * @grid: 3x3 grid
+ */
+static void print_grid(int grid[3][3])
+{
+    int i, j;
+
+    printf("=\n");
+    for (i = 0; i < SIZE; i++)
+    {
+        for (j = 0; j < SIZE; j++)
+        {
+            if (j)
+                printf(" ");
+            printf("%d", grid[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+/**
+ * is_unstable - Checks if a sandpile is unstable
+ * @grid: 3x3 grid
+ *
+ * Return: 1 if unstable, 0 if stable
+ */
+static int is_unstable(int grid[3][3])
+{
+    int i, j;
+
+    for (i = 0; i < SIZE; i++)
+    {
+        for (j = 0; j < SIZE; j++)
+        {
+            if (grid[i][j] > 3)
+                return (1);
+        }
+    }
+    return (0);
+}
+
+/**
+ * topple - Performs one toppling operation
+ * @grid: 3x3 grid to update
+ */
+static void topple(int grid[3][3])
+{
+    int temp[3][3] = {{0}};
+    int i, j;
+
+    for (i = 0; i < SIZE; i++)
+    {
+        for (j = 0; j < SIZE; j++)
+        {
+            if (grid[i][j] > 3)
+            {
+                temp[i][j] -= 4;
+                if (i > 0)
+                    temp[i - 1][j] += 1;
+                if (i < SIZE - 1)
+                    temp[i + 1][j] += 1;
+                if (j > 0)
+                    temp[i][j - 1] += 1;
+                if (j < SIZE - 1)
+                    temp[i][j + 1] += 1;
+            }
+        }
+    }
+
+    for (i = 0; i < SIZE; i++)
+    {
+        for (j = 0; j < SIZE; j++)
+        {
+            grid[i][j] += temp[i][j];
+        }
+    }
+}
+
+/**
+ * sandpiles_sum - Computes the sum of two sandpiles
+ * @grid1: First 3x3 grid (will be updated in-place)
+ * @grid2: Second 3x3 grid
+ */
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+    int i, j;
+
+    for (i = 0; i < SIZE; i++)
+    {
+        for (j = 0; j < SIZE; j++)
+        {
+            grid1[i][j] += grid2[i][j];
+        }
+    }
+
+    while (is_unstable(grid1))
+    {
+        print_grid(grid1);
+        topple(grid1);
+    }
+}
