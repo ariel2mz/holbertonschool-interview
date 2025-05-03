6eee031837eee05f1a5845b96bfc69c7a01f7f78
ariel <ariel@gmail.com>
2025-05-03 01:25:45 -0300

    dasd

--- sandpiles/sandpiles.h
+++ sandpiles/sandpiles.h
@@ -2,8 +2,8 @@
 #define SANDPILES_H
 
 /* Function prototype */
-static void print_grid(int grid[3][3]);
-static int is_unstable(int grid[3][3]);
-static void topple(int grid[3][3]);
+void print_grid(int grid[3][3]);
+int is_unstable(int grid[3][3]);
+void topple(int grid[3][3]);
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 #endif /* SANDPILES_H */
