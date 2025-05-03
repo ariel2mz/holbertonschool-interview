a50483a4eb9fa7315513f3a9da2de6412798ea0e
ariel <ariel@gmail.com>
2025-05-03 01:24:44 -0300

    asdd

---
+++ sandpiles/sandpiles.h
@@ -0,0 +1,9 @@
+#ifndef SANDPILES_H
+#define SANDPILES_H
+
+/* Function prototype */
+static void print_grid(int grid[3][3]);
+static int is_unstable(int grid[3][3]);
+static void topple(int grid[3][3]);
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+#endif /* SANDPILES_H */
