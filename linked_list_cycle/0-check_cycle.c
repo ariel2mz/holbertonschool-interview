242d1dd524bde1c912f41a5f896c95968f7a8948
ariel <ariel@gmail.com>
2025-05-20 15:12:42 -0300

    sadsadsa

---
+++ linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * check_cycle - prints all elements of a listint_t list
+ * @list: pointer to head of list
+ * Return: number of nodes
+ */
+int check_cycle(listint_t *list)
+{
+listint_t *uno = list;
+listint_t *dos = list;
+
+while (dos != NULL dos dos->next != NULL)
+{
+uno = uno->next;
+dos = dos->next->next;
+if (dos == uno)
+return 1;
+}
+return 0;
+}
