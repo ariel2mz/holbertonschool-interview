36fa3f6641d9ca7ad40ad9f8aed139b77a7b09af
ariel <ariel@gmail.com>
2025-06-21 15:59:52 -0300

    sasa

--- menger/0-menger.c
+++ menger/0-menger.c
@@ -10,14 +10,14 @@
  */
 static int hole(int row, int col)
 {
-    while (row > 0 || col > 0)
-    {
-        if (row % 3 == 1 && col % 3 == 1)
-            return 1;  // This is a hole
-        row /= 3;
-        col /= 3;
-    }
-    return 0;
+while (row > 0 || col > 0)
+{
+if (row % 3 == 1 && col % 3 == 1)
+return 1;
+row /= 3;
+col /= 3;
+}
+return 0;
 }
 
 /**
@@ -26,21 +26,21 @@ static int hole(int row, int col)
  */
 void menger(int level)
 {
-    if (level < 0)
-        return;
+if (level < 0)
+return;
 
-    int size = pow(3, level);
-    int row, col;
+int size = pow(3, level);
+int row, col;
 
-    for (row = 0; row < size; row++)
-    {
-        for (col = 0; col < size; col++)
-        {
-            if (hole(row, col))
-                putchar(' ');
-            else
-                putchar('#');
-        }
-        putchar('\n');
-    }
+for (row = 0; row < size; row++)
+{
+for (col = 0; col < size; col++)
+{
+if (hole(row, col))
+putchar(' ');
+else
+putchar('#');
+}
+putchar('\n');
+}
 }
