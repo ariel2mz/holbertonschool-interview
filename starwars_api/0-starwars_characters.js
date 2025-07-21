1ca748e1574a6738d1484d31d7d4c4803c5c5128
ariel <ariel@gmail.com>
2025-07-21 02:58:34 +0000

    sdsd

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -4,19 +4,20 @@ const request = require('request');
 const movieId = process.argv[2];
 const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
-request(apiUrl, (error, response, body) => {
+request(apiUrl, function (error, response, body) {
+  if (error) return;
   const filmData = JSON.parse(body);
   const characterUrls = filmData.characters;
 
   function getCharacter(index) {
     if (index >= characterUrls.length) return;
     
-    request(characterUrls[index], (err, res, charBody) => {
-      const characterData = JSON.parse(charBody);
-      console.log(characterData.name);
+    request(characterUrls[index], function (err, res, charBody) {
+      if (err) return;
+      console.log(JSON.parse(charBody).name);
       getCharacter(index + 1);
     });
   }
-  
+
   getCharacter(0);
 });
