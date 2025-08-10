191dfb6ef533aeb139b79edbbdc3e00e4de4c8a8
ariel <ariel@gmail.com>
2025-08-10 16:06:26 -0300

    SADAS

--- advanced_binary_search/search_algos.h
+++ advanced_binary_search/search_algos.h
@@ -2,5 +2,7 @@
 #define BINARY_TREES_H
 
 #include <stddef.h>
-
+void print_array(int *array, size_t left, size_t right);
+int recursive_binary(int *array, size_t left, size_t right, int value);
+int advanced_binary(int *array, size_t size, int value);
 #endif
