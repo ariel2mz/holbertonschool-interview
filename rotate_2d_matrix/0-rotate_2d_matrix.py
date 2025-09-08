95e4a29226afbb8fd6354cd541ee6d9f14c69b65
ariel <ariel@gmail.com>
2025-09-07 23:29:19 -0300

    asda

---
+++ rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+2dmatrix
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """
+    sadsadsadsa
+    """
+    n = len(matrix)
+
+    for i in range(n):
+        for j in range(i + 1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    for i in range(n):
+        matrix[i].reverse()
