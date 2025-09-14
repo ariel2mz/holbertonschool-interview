f8f641beda25f33712d5ac78d197054ac29ac340
ariel <ariel@gmail.com>
2025-09-14 16:36:12 +0000

    as

---
+++ find_the_loop/0-find_loop.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - aaaaa
+ * @head: aaaaaaaa
+ *
+ * Return: aaaaaaa
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+listint_t *antes, *dsps;
+
+if (!head || !head->next)
+return (NULL);
+
+antes = head;
+dsps = head;
+while (dsps && dsps->next)
+{
+antes = antes->next;
+dsps = dsps->next->next;
+
+if (antes == dsps)
+{
+antes = head;
+while (antes != dsps)
+{
+antes = antes->next;
+fast = fast->next;
+}
+return (antes);
+}
+}
+return (NULL);
+}
