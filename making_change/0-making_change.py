8f3a17093d1cba2eb3a07d66a7d38d19eca38c24
ariel <ariel@gmail.com>
2025-09-27 11:29:44 -0300

    sada

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -13,9 +13,9 @@ def makeChange(coins, total):
     lista = [total + 1] * (total + 1)
     lista[0] = 0
 
-    for amount in range(1, total + 1):
+    for cant in range(1, total + 1):
         for coin in coins:
-            if coin <= amount:
-                lista[amount] = min(lista[amount], lista[amount - coin] + 1)
+            if coin <= cant:
+                lista[cant] = min(lista[cant], lista[cant - coin] + 1)
 
     return lista[total] if lista[total] <= total else -1
