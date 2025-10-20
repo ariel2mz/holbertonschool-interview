f166f0c76da164e2b58117090ecdd16ab9e6233f
ariel <ariel@gmail.com>
2025-10-20 05:47:01 +0000

    sadas

--- avl_trees/binary_trees.h
+++ avl_trees/binary_trees.h
@@ -21,5 +21,9 @@ struct binary_tree_s *right;
 
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s avl_t;
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+void binary_tree_print(const binary_tree_t *tree);
+void binary_tree_delete(binary_tree_t *tree);
+int binary_tree_is_avl(const binary_tree_t *tree);
 
 #endif
